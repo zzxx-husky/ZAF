850e81ef1553b7234b431e36d35a22f9b13c79dd
byli <24curiosity@gmail.com>
2022-05-04 15:47:37 +0800

    Fix compilation error on Mac

--- core/zaf/thread_utils.hpp
+++ core/zaf/thread_utils.hpp
@@ -1,12 +1,20 @@
 #pragma once
 
-#include <sched.h>
 #include <string>
+
+#ifdef __APPLE__
+#include <pthread.h>
+#endif
+
+#ifdef __linux__
+#include <sched.h>
+#endif
 
 #include "zaf_exception.hpp"
 
 namespace zaf {
 namespace thread {
+
 inline static std::string get_name() {
   auto native_handle = pthread_self();
   std::string name;
@@ -31,13 +39,17 @@ inline static std::string get_name() {
  */
 [[maybe_unused]]
 inline static void migrate_to_cpu(int target_cpu) {
+#ifdef __linux__
   auto native_handle = pthread_self();
   std::string t_name = get_name();
-  // auto old_cpu = sched_getcpu();
   cpu_set_t old_cpuset, cpuset;
   auto rc = pthread_getaffinity_np(native_handle, sizeof(cpu_set_t), &old_cpuset);
   if (rc != 0) {
     throw ZAFException("Failed to get cpu set for ", t_name, ". Error: ", rc);
+  }
+  if (!CPU_ISSET(target_cpu, &old_cpuset)) {
+    throw ZAFException("Target cpu (i.e., ", target_cpu, ") to migrate must be "
+      "a subset of the current cpu set.");
   }
   CPU_ZERO(&cpuset);
   CPU_SET(target_cpu, &cpuset);
@@ -51,16 +63,22 @@ inline static void migrate_to_cpu(int target_cpu) {
   if (rc != 0) {
     throw ZAFException("Failed to reset cpu affinity for ", t_name, ". Error: ", rc);
   }
+#endif
 }
 
+[[maybe_unused]]
 inline static void set_name(const std::string& t_name) {
-  auto native_handle = pthread_self();
   if (t_name.size() + 1 > 16) {
     throw ZAFException(t_name, " is too long. Thread name in linux is not "
       "allowed to be larger than 16, including the terminaing null byte.");
   }
   auto old_t_name = get_name(); 
-  auto rc = pthread_setname_np(native_handle, t_name.c_str());
+#ifdef __APPLE__
+  auto rc = pthread_setname_np(t_name.c_str());
+#endif
+#ifdef __linux__
+  auto rc = pthread_setname_np(pthread_self(), t_name.c_str());
+#endif
   if (rc != 0) {
     throw ZAFException("Failed to set the name of thread ", old_t_name,
       " to ", t_name, ". Error: ", rc);
